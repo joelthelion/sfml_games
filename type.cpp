e568cfec781fcfe872368c47e15abb9a627ad07f
Joël Schaerer <joel.schaerer@gmail.com>
2013-11-30 18:07:41 +0100

    letters

--- type.cpp
+++ type.cpp
@@ -1,14 +1,49 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <cmath>
+#include <map>
 #include <iostream>
 
 using namespace sf;
+
+class LetterSounds {
+		public:
+				static void play(char letter) {
+						snd.setBuffer(sounds[letter]);
+						snd.play();
+				}
+		protected:
+				typedef std::map<char,SoundBuffer> SoundMap;
+				static SoundMap init_map() {
+						SoundMap map;
+						for (char i='a';i<='z';++i) {
+								std::cout << "init " << i << std::endl;
+								SoundBuffer b;
+								std::string filename = "x.ogg";
+								filename[0] = i;
+								b.loadFromFile("lettres/"+filename);
+								map[i] = b;  
+						}
+						return map;
+				}
+				static SoundMap sounds;
+				static Sound snd;
+};
+
+LetterSounds::SoundMap LetterSounds::sounds = LetterSounds::init_map();
+Sound LetterSounds::snd;
 
 
 int main()
 {
     // create the window
-    RenderWindow window(VideoMode(800, 600), "My window");
+    RenderWindow window(VideoMode::getFullscreenModes()[0], "My window", Style::Fullscreen);
+    //RenderWindow window(VideoMode(800, 600), "My window");
+		std::string text;
+		Font font;
+		font.loadFromFile("/usr/share/fonts/TTF/arial.ttf");
+		Text txt;
+		txt.setFont(font);
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -26,7 +61,13 @@ int main()
 				case Event::TextEntered:
 					{
 						String str(event.text.unicode);
-						std::cout << str.toAnsiString() << std::endl;
+						if (str == "[") { return 0; }
+						char letter = (char)str[0];
+						if (letter >= 'a' && letter <= 'z') {
+								std::cout << letter << std::endl;
+								LetterSounds::play(letter);
+								text += toupper(letter);
+						}
 						break;
 					}
 				default:
@@ -38,6 +79,9 @@ int main()
         window.clear(Color::Green);
 
         // draw everything here...
+				txt.setString(text);
+				window.draw(txt);
+				
 
         // end the current frame
         window.display();
